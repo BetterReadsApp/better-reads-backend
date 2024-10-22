50b2fb7e45c07c69bf4781aeb2c62761832f52ba
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-10-22 18:53:27 -0300

    fix: ahora para autenticar al usuario se pasa el user_id a traves del header auth y no por el body

--- api/model/shelf.py
+++ api/model/shelf.py
@@ -4,10 +4,10 @@ from sqlmodel import SQLModel, Field
 
 class ShelfForm(SQLModel):
     name: str
-    user_id: int = Field(default=None, foreign_key="users.id")
 
 
 class Shelf(ShelfForm, table=True):
     __tablename__ = "shelves"
 
     id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: Optional[int] = Field(default=None, foreign_key="users.id")
