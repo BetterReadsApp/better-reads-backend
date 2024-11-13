81c759c9646d42eda6f9da516718ca2b6e5fb190
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-11-13 15:49:36 -0300

    fix: agregadas listas de rated_books y reviewed_books a get user by id

--- api/formatters/user_formatter.py
+++ api/formatters/user_formatter.py
@@ -13,6 +13,36 @@ class UserFormatter:
         user_dict["shelves"] = list(map(ShelfMini.model_validate, user.shelves))
         user_dict["followers"] = list(map(UserMini.model_validate, user.followers))
         user_dict["following"] = list(map(UserMini.model_validate, user.following))
+        user_dict["rated_books"] = cls.format_ratings(user.rated_books)
+        user_dict["reviewed_books"] = cls.format_reviews(user.reviewed_books)
         user_dict.pop("password")
         user_dict.pop("email")
         return user_dict
+
+    @classmethod
+    def format_ratings(cls, ratings):
+        return list(
+            map(
+                lambda rating: {
+                    "value": rating.value,
+                    "book_id": rating.book_id,
+                    "title": rating.book.title,
+                    "cover_image_url": rating.book.cover_image_url,
+                },
+                ratings,
+            )
+        )
+
+    @staticmethod
+    def format_reviews(reviews):
+        return list(
+            map(
+                lambda review: {
+                    "review": review.review,
+                    "book_id": review.book_id,
+                    "title": review.book.title,
+                    "cover_image_url": review.book.cover_image_url,
+                },
+                reviews,
+            )
+        )
